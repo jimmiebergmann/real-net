871fbac72d540c3686a701c333a53abf87f34081
Jimmie Bergmann <jimmiebergmann@gmail.com>
2018-02-28 19:37:10 +0100

    Fixed new travis script for running test.

--- test/run_test.sh
+++ test/run_test.sh
@@ -4,6 +4,7 @@ warning_kinds="definite,indirect"
 
 script="valgrind --leak-check=full --track-origins=yes --errors-for-leak-kinds=${error_kinds} --show-leak-kinds=${warning_kinds} --run-libc-freeres=no --error-exitcode=1 ./${program}"
 
+ifconfig
 echo "Running:"
 echo ${script}
 ${script}
